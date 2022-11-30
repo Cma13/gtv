d6e5c071a0dae5eb17553f2d47bb93042e818c89
Carlos Molina Alcolea <carlosmolinaalcolea2000@gmail.com>
2022-11-30 18:33:35 +0100

    subrayado añadido a los links de navegacion

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -6,28 +6,28 @@
         </div>
         <div class="w-full md:w-auto flex items-center" id="mobile-menu">
             <ul class="flex flex-col mt-4 md:flex-row md:space-x-6 md:mt-0 md:text-md md:font-bold">
-                <li>
+                <li class="{{ request()->routeIs('points.index') ? 'border-b-4 border-sky-900' : '' }}">
                     <a href="{{ route('points.index') }}"
-                        class="block py-2 pr-4 pl-3 {{ request()->routeIs('points.index') ? 'text-sky-900' : 'text-white' }} hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-gray-200 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Puntos
+                        class="block py-2 pr-4 pl-3 {{ request()->routeIs('points.index') ? 'text-sky-900 border-b-4 border-gray-500' : 'text-white' }} hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-gray-200 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Puntos
                         de Interés</a>
                 </li>
                 @hasanyrole('Administrador|Profesor')
-                    <li>
+                    <li class="{{ request()->routeIs('places.index') ? 'border-b-4 border-sky-900' : '' }}">
                         <a href="{{ route('places.index') }}"
                             class="block py-2 pr-4 pl-3 {{ request()->routeIs('places.index') ? 'text-sky-900' : 'text-white' }} border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-gray-200 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Lugares</a>
                     </li>
                 @endhasanyrole
-                <li>
+                <li class="{{ request()->routeIs('videos.index') ? 'border-b-4 border-sky-900' : '' }}">
                     <a href="{{ route('videos.index') }}"
                         class="block py-2 pr-4 pl-3 {{ request()->routeIs('videos.index') ? 'text-sky-900' : 'text-white' }} border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-gray-200 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Vídeos</a>
                 </li>
-                <li>
+                <li class="{{ request()->routeIs('photographies.index') ? 'border-b-4 border-sky-900' : '' }}">
                     <a href="{{ route('photographies.index') }}"
                         class="block py-2 pr-4 pl-3 {{ request()->routeIs('photographies.index') ? 'text-sky-900' : 'text-white' }} border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-gray-200 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Fotografías</a>
                 </li>
 
                 @hasanyrole('Administrador|Profesor')
-                    <li>
+                    <li class="{{ request()->routeIs('thematic-areas.index') ? 'border-b-4 border-sky-900' : '' }}">
                         <a href="{{ route('thematic-areas.index') }}"
                             class="block py-2 pr-4 pl-3 {{ request()->routeIs('thematic-areas.index') ? 'text-sky-900' : 'text-white' }} border-b
                             border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-gray-200
@@ -36,20 +36,19 @@
                             Áreas temáticas
                         </a>
                     </li>
-                    <li>
+                    <li class="{{ request()->routeIs('visit.index') ? 'border-b-4 border-sky-900' : '' }}">
                         <a href="{{ route('visit.index') }}"
                             class="block py-2 pr-4 pl-3 {{ request()->routeIs('visit.index') ? 'text-sky-900' : 'text-white' }} border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-gray-200 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Visitas</a>
                     </li>
                 @endhasanyrole
 
                 @role('Administrador')
-                    <li>
+                    <li class="{{ request()->routeIs('video-items.index') ? 'border-b-4 border-sky-900' : '' }}">
                         <a href="{{ route('video-items.index') }}"
                             class="block py-2 pr-4 pl-3 {{ request()->routeIs('video-items.index') ? 'text-sky-900' : 'text-white' }} border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-gray-200 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Vídeo
                             Items</a>
                     </li>
-
-                    <li>
+                    <li class="{{ request()->routeIs('users.index') ? 'border-b-4 border-sky-900' : '' }}">
                         <a href="{{ route('users.index') }}"
                             class="block py-2 pr-4 pl-3 {{ request()->routeIs('users.index') ? 'text-sky-900' : 'text-white' }} border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-gray-200 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Usuarios</a>
                     </li>
@@ -58,7 +57,7 @@
 
             <div class="ml-8">
                 @auth
-                   @livewire('admin.user-profile')
+                    @livewire('admin.user-profile')
                 @endauth
             </div>
         </div>
