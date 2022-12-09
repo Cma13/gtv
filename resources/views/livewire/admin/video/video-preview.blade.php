384163ede5f721a117724b77b2aecfdc23784da9
Carlos Molina Alcolea <carlosmolinaalcolea2000@gmail.com>
2022-12-09 13:24:21 +0100

    corregida vista preview Video

--- resources/views/livewire/admin/video/video-preview.blade.php
+++ resources/views/livewire/admin/video/video-preview.blade.php
@@ -1,5 +1,4 @@
 <div class="my-10">
-    {{ $route }}
     <video class="mx-auto w-4/5" controls>
         <source src="{{ $route }}" type="video/mp4">
         Your browser does not support the video tag.
